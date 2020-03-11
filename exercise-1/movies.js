e54ed05539d4f1f57eed65a8522a0f2a82d0ec6f
Pepe García <pepe@pepegar.com>
2020-03-11 18:43:14 +0100

    changes from class

--- exercise-1/movies.js
+++ exercise-1/movies.js
@@ -3,7 +3,24 @@
 const randomMovieUrl = "https://api.rand.fun/movies/title";
 const omdbUrl = "http://www.omdbapi.com/?apikey=ca3c636a&t="
 
-// Probably we should start with something like: fetch(randomMovieUrl)...
+function getMovieInfo(json) {
+  return fetch(omdbUrl + json.result);
+}
 
 function addInfoToHTML(data) {
+  let h1 = document.querySelector("#random-movie h1");
+  h1.innerHTML = data.Title;
+
+  let img = document.querySelector("#random-movie img");
+  img.setAttribute("src", data.Poster);
+
+  let description = document.querySelector("#random-movie .description");
+  description.innerHTML = data.Plot;
 }
+
+fetch(randomMovieUrl)
+  .then(response => response.json())
+  .then(getMovieInfo)
+  .then(response => response.json())
+  .then(addInfoToHTML)
+
