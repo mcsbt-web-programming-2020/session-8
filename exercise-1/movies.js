242aea22cf3a8109d37d5e0c8b1852cf7a9f2ea5
Pepe García <pepe@pepegar.com>
2020-03-11 16:55:21 +0100

    prepare exercises for class

--- exercise-1/movies.js
+++ exercise-1/movies.js
@@ -3,21 +3,7 @@
 const randomMovieUrl = "https://api.rand.fun/movies/title";
 const omdbUrl = "http://www.omdbapi.com/?apikey=ca3c636a&t="
 
-fetch(randomMovieUrl)
-    .then(response => response.json())
-    .then(json => fetch(omdbUrl + json.result))
-    .then(response => response.json())
-    .then(json => addInfoToHTML(json))
-    .catch(err => console.log(err))
-
+// Probably we should start with something like: fetch(randomMovieUrl)...
 
 function addInfoToHTML(data) {
-    let h1 = document.querySelector("#random-movie h1");
-    h1.innerHTML = data.Title;
-
-    let image = document.querySelector("#random-movie img");
-    image.setAttribute("src", data.Poster);
-
-    let description = document.querySelector("#random-movie .description");
-    description.innerHTML = data.Plot;
 }
